a7c583385dcbc75c883ff16b6122d7e5f1d2d5c6
Wilermo <william.dmo.sec@gmail.com>
2024-05-11 17:35:26 -0500

    Configuración URLs QA para marketing

--- src/app/service/marketing.service.ts
+++ src/app/service/marketing.service.ts
@@ -12,23 +12,23 @@ export class MarketingService {
   constructor(private http: HttpClient) {}
 
   findAll() {
-    return this.http.get<MarketingDto[]>("http://localhost:8762/possibleclient/list");
+    return this.http.get<MarketingDto[]>("https://canelaapigatewayback-qa.up.railway.app/possibleclient/list");
   }
 
   findAllStatuses() {
-    return this.http.get<Marketingstatus[]>("http://localhost:8762/marketingstatus/list");
+    return this.http.get<Marketingstatus[]>("https://canelaapigatewayback-qa.up.railway.app/marketingstatus/list");
   }
 
   saveAll(empresas: MarketingDto[]) {
-    return this.http.post<MarketingDto[]>("http://localhost:8762/possibleclient/saveAll", empresas);
+    return this.http.post<MarketingDto[]>("https://canelaapigatewayback-qa.up.railway.app/possibleclient/saveAll", empresas);
   }
 
   saveStatus(estado: Marketingstatus) {
-    return this.http.post<Marketingstatus>("http://localhost:8762/marketingstatus/save", estado);
+    return this.http.post<Marketingstatus>("https://canelaapigatewayback-qa.up.railway.app/marketingstatus/save", estado);
   }
 
   delete(id: number) {
-    return this.http.delete(`http://localhost:8762/marketingstatus/delete/${id}`);
+    return this.http.delete(`https://canelaapigatewayback-qa.up.railway.app/marketingstatus/delete/${id}`);
 
   }
 }
