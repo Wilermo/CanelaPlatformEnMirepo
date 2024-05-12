44f4a1058b567d1f2c8338c117352222d933fec6
Sebasgmv <sebastiangmv@hotmail.com>
2024-05-12 05:28:58 -0500

    Conectada al API Gateway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   authURL: 'https://canelaapigatewayback-qa.up.railway.app/api/talentsoft/auth',
   secretKey: 'ThisIsASecretKey',
   //QaURL: 'https://talentsoftdespidos-qa.up.railway.app',
-  URL: 'http://localhost:8080', // Cambiarla por el API GATEWAY A BACK
-};
+  URL: 'https://canelaapigatewayback-qa.up.railway.app', // Cambiarla por el API GATEWAY A BACK 
+/*   URL: 'http://localhost:8080', // Cambiarla por el API GATEWAY A BACK https://canelaapigatewayback-qa.up.railway.app
+ */};
