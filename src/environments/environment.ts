cd5e482c07e0079a7891b591710fb5a8105a8808
Sebasgmv <sebastiangmv@hotmail.com>
2024-05-12 15:26:18 -0500

    Conectado a railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   authURL: 'https://canelaapigatewayback-qa.up.railway.app/api/talentsoft/auth',
   secretKey: 'ThisIsASecretKey',
   //QaURL: 'https://talentsoftdespidos-qa.up.railway.app',
-  URL: 'http://localhost:8080', // Cambiarla por el API GATEWAY A BACK
+  URL: 'https://modulocuentasplan-qa.up.railway.app', // Cambiarla por el API GATEWAY A BACK
 };
