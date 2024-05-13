ec161501853172276510d6368941b4ed677b5ba4
Wilermo <william.dmo.sec@gmail.com>
2024-05-12 21:58:59 -0500

    Conexión back

--- src/app/components/permisos-usuario/permisos-usuario.component.ts
+++ src/app/components/permisos-usuario/permisos-usuario.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UsuarioPermisoDto } from 'src/app/model/usuario-permiso-dto';
 import { PermisosUsuarioService } from 'src/app/service/permisos-usuario.service';
+import { User } from 'src/app/shared/model/auth/user';
+import { UserService } from '../../shared/model/user.service';
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-permisos-usuario',
@@ -15,17 +18,119 @@ export class PermisosUsuarioComponent implements OnInit {
   ) {}
 
   usuarios: UsuarioPermisoDto[] | undefined;
+  roles: string[] | undefined;
+  rolesPorUsuario: { [key: string]: string[] } = {};
 
+  username: string | undefined;
   ngOnInit(): void {
-    this.permisosUsuarioService
-      .findAllUsers()
-      .subscribe((users) => (this.usuarios = users));
+    Swal.fire({
+      icon: 'warning',
+      title: 'Gestión de usuarios',
+      text: "No olvide guardar los cambios al finalizar",
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#963e6c',
+    });
+    this.permisosUsuarioService.findAllUsers().subscribe((users) => {
+      this.usuarios = users;
+      this.usuarios.forEach((user) => {
+        this.rolesPorUsuario[user.username] = user.roles;
+      });
+    });
   }
 
   enableMarketing(id: number) {}
   enableFinanzas(id: number) {}
   enableServer(id: number) {}
 
+  public handleCheckboxChange(
+    event: Event,
+    username: string,
+    rol: string
+  ): void {
+    const target = event.target as HTMLInputElement;
+    if (target) {
+      const isChecked = target.checked;
+      this.actualizarRoles(username, rol, isChecked);
+    } else {
+      // Manejar el caso en que target es null (si es necesario)
+      console.error('El target es null en el evento del checkbox.');
+    }
+  }
+
+  actualizarRoles(username: string, rol: string, isChecked: boolean): void {
+    // Verificar si el usuario ya tiene este rol
+    this.username = username;
+    if (isChecked) {
+      this.rolesPorUsuario[username].push(rol);
+    }else{
+      let rolesFinales : string[] = [];
+      this.rolesPorUsuario[username].forEach((rolInner) => {
+          if (rolInner != rol) {
+            rolesFinales.push(rolInner);
+          }
+        }
+      )
+      this.rolesPorUsuario[username] = rolesFinales;
+    }
+
+    console.log(
+      "Roles actualizados para el usuario con ID : " +
+      this.username + " Sus roles son: " +
+    this.rolesPorUsuario[username]
+  );
+  }
+
+  guardarCambios(){
+    if(this.usuarios != undefined){
+      this.usuarios.forEach((user) => {
+        user.roles = this.rolesPorUsuario[user.username] ;
+      });
+      this.permisosUsuarioService.saveAllUsers(this.usuarios).subscribe(result => {
+        if(!result){
+          Swal.fire({
+            icon: 'error',
+            title: 'Gestión de usuarios',
+            text: "Ha ocurrido un error al guardar los usuarios",
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#963e6c',
+          });
+        }else{
+          Swal.fire({
+            icon: 'success',
+            title: 'Gestión de usuarios',
+            text: "Cambios guardados correctamente",
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#963e6c',
+          });
+        }
+      });
+    }
+
+
+
+  }
+  /*
+  enviarDatosRoles(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      console.log('Datos que se envían:', this.nuevoUsuarioAuth);
+
+      this.userService
+        .createUser(this.nuevoUsuarioAuth, this.nuevoUsuarioAuth.role, token)
+        .subscribe({
+          next: (data) => {
+            console.log('Respuesta recibida:', data);
+            this.router.navigate(['/canela/permisos']);
+          },
+          error: (err) => {
+            console.error('Error al enviar datos:', err);
+          },
+        });
+    } else {
+      console.error('No se encontró el token de autenticación');
+    }
+  }
+  */
   login() {
     this.router.navigate(['/']);
   }
